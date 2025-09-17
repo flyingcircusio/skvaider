96f38ceefdba0f3e03d2995a51e073a5bd476035
Christian Theune <ct@flyingcircus.io>
2025-09-17 13:38:28 +0200

    tooling: fix ollama instances colliding, add model caching

--- devenv.nix
+++ devenv.nix
@@ -7,13 +7,17 @@
 
   enterTest = ''
     wait_for_port 11435
+    wait_for_port 11436
     # Wait for model (defined in processes:ollama) to be available
-    until ${lib.getExe pkgs.curl} -s http://127.0.0.1:11435/v1/models | ${lib.getExe pkgs.yq-go} -e ".data"; do sleep 0.5; done
+    until ${lib.getExe pkgs.curl} -s http://127.0.0.1:11435/v1/models/nomic-embed-text:v1.5 | grep created; do sleep 5; done
+    until ${lib.getExe pkgs.curl} -s http://127.0.0.1:11435/v1/models/gemma3:1b | grep created; do sleep 5; done
+    until ${lib.getExe pkgs.curl} -s http://127.0.0.1:11436/v1/models/nomic-embed-text:v1.5 | grep created; do sleep 5; done
+    until ${lib.getExe pkgs.curl} -s http://127.0.0.1:11436/v1/models/gemma3:1b | grep created; do sleep 5; done
     run-tests
   '';
 
   env = {
-    OLLAMA_HOST = "127.0.0.1:11435";
+    OLLAMA_HOST = "127.0.0.1:11435"; # use the first ollama as the default
     PYTHONUNBUFFERED = "1"; # makes output from subprocesses in tests more reliably visible
   };
 
@@ -36,6 +40,7 @@
         export OLLAMA_NEW_ENGINE="1"
         export OLLAMA_NEW_ESTIMATES="1"
         export OLLAMA_KEEP_ALIVE="-1"
+        export OLLAMA_MODELS=".ollama1/models"
 
         ollama serve&
         timeout 15 bash -c "until ${lib.getExe pkgs.curl} http://localhost:11435 -s; do sleep 0.5; done"
@@ -56,6 +61,7 @@
         export OLLAMA_NEW_ENGINE="1"
         export OLLAMA_NEW_ESTIMATES="1"
         export OLLAMA_KEEP_ALIVE="-1"
+        export OLLAMA_MODELS=".ollama2/models"
 
         ollama serve&
         timeout 15 bash -c "until ${lib.getExe pkgs.curl} http://localhost:11436 -s; do sleep 0.5; done"
