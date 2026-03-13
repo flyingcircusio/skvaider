58c4c5b500d88f096d72f8c3d3b1729f5e4818ff
Christian Theune <ct@flyingcircus.io>
2026-03-13 09:31:23 +0100

    proxy/auth: cache lookups to reduce a serious performance bottleneck
    I noticed that 100 requests coming in at once are easily taking
    30s to get passed to the backends. Seems like the current way we
    authenticate requests requires ~300ms and likely has a db contention
    issue.

--- src/skvaider/auth.py
+++ src/skvaider/auth.py
@@ -1,6 +1,7 @@
 import base64
 import binascii
 import json
+import time
 from json import JSONDecodeError
 from typing import Annotated
 
@@ -20,10 +21,39 @@ class AuthTokens(aramaki.Collection):
     collection = "fc.directory.ai.token"
 
 
+class Cache:
+    # XXX turn this into a feature of the collection
+    # avoid hitting the session at all if we have a valid cache
+    # Also allow caching negative results.
+
+    TTL = 300
+
+    def __init__(self):
+        self.cache: dict[str, float] = {}
+
+    def __contains__(self, key: str):
+        now = time.time()
+        if key not in self.cache:
+            return False
+        if self.cache[key] < now:
+            del self.cache[key]
+            return False
+        return True
+
+    def add(self, key: str):
+        self.cache[key] = time.time() + self.TTL
+
+
+cache = Cache()  # XXX turn into service
+
+
 async def verify_token(
     credentials: Annotated[HTTPAuthorizationCredentials, Depends(_bearer_auth)],
     services: svcs.fastapi.DepContainer,
 ) -> None:
+    if credentials.credentials in cache:
+        return
+
     # XXX There's a lot of type issues going on here, because the mechanics of passing through
     # the correct types from services.aget() with a factory for a context manager that isn't entered
     # are just too hard for the type system for now afaict.
@@ -45,6 +75,7 @@ async def verify_token(
         try:
             assert isinstance(db_token["secret_hash"], str)
             hasher.verify(db_token["secret_hash"], client_token["secret"])  # type: ignore
+            cache.add(credentials.credentials)
         # We could specify explicit exceptions here but go the safe route and just catch all in case the lib addes one
         except Exception:
             raise HTTPException(401, detail="Bad authentication")
