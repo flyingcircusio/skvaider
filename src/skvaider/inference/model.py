6011de5a96e6c508203edea540d0f9ee05c1b20e
Christian Theune <ct@flyingcircus.io>
2026-04-14 13:12:33 +0200

    bugfix: allow properly cleaning up our subprocess.

--- src/skvaider/inference/model.py
+++ src/skvaider/inference/model.py
@@ -353,6 +353,9 @@ class Model(ABC):
             stdout=log_file,
             stderr=log_file,
             env=process_env,
+            # XXX we may need to consider improving our termination *if* the cleanup
+            # of a new session should be unreliable.
+            start_new_session=True,
         )
         log_file.close()  # child inherited the FD; we no longer need our copy
         try:
